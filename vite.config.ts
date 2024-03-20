b9be1f5da0c874db9673952ca3bdb724246925e6
smnm1998 <dltpals7498@gmail.com>
2024-03-20 10:19:45 +0900

    fix: vite 설치 오류 해결

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; // @vitejs/plugin-react-swc를 import
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+});
