2a392b2ce5817bbd7b7dafc23ba162f099b03861
smnm1998 <dltpals7498@gmail.com>
2024-04-08 13:22:48 +0900

    chore: 메인페이지 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import './App.css';
 import { Provider } from 'react-redux';
 import store from './modules/Store';
 import './assets/styles/Darkmode.css'; // 이 CSS 파일에는 body 요소의 클래스명이 설정되어 있어야 합니다.
-import Books from './routes/Books';
+import Mainpage from './routes/Mainpage';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
       <div className="App">
-        <Books />
+        <Mainpage />
       </div>
     </Provider>
   );
