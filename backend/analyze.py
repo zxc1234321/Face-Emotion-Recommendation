170710b993b5ed4f19f129eb2f4a848753ec2fe8
zxc1234321 <spcle2412@gmail.com>
2024-05-27 20:46:32 +0900

    Feature: DeepFace를 활용한 인공지능 로직 구현

---
+++ backend/analyze.py
@@ -0,0 +1,16 @@
+from deepface import DeepFace
+import sys
+import json
+
+def analyze_image(image_path):
+    objs = DeepFace.analyze(img_path=image_path, actions=['emotion'], enforce_detection=False)
+    emotion = objs[0]['dominant_emotion']
+    return {'emotion': emotion}
+
+if __name__ == "__main__":
+    image_path = sys.argv[1]
+    try:
+        result = analyze_image(image_path)
+        print(json.dumps(result))  # JSON 형식으로 결과 출력
+    except Exception as e:
+        print(json.dumps({"error": str(e)}))  # 오류 발생 시 JSON 형식으로 출력
